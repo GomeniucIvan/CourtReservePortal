dceb0a3b022a0bde138cd5de140807fa9ffc3157
GomeniucIvan <gomeniucivan@gmail.com>
2024-08-15 22:14:01 +0300

    formik

--- src/pages/account/login/Login.GetStarted.jsx
+++ src/pages/account/login/Login.GetStarted.jsx
@@ -3,42 +3,63 @@ import {Button, Form, Input} from "antd-mobile";
 import {useNavigate} from "react-router-dom";
 import { useFormik } from 'formik';
 import {useFormikContext} from "../../../context/FormikProvider.jsx";
+import * as Yup from "yup";
 
 function LoginGetStarted() {
     const { setFormikData } = useFormikContext();
     const navigate = useNavigate();
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: ''
+    const startInitialValues = {
+        email: '',
+        password: ''
+    };
+
+    const startValidationSchema = Yup.object({
+        email: Yup.string().required('Email or Username is required.')
+    });
+    
+    const startFormik = useFormik({
+        initialValues: startInitialValues,
+        validationSchema: startValidationSchema,
+        onSubmit: async (values, { setStatus, setSubmitting }) => {
+            setSubmitting(true);
+            
+            setTimeout(function (){
+                setSubmitting(true);
+                
+                setFormikData(values);
+                console.log('Form submitted:', values);
+                navigate('/login-account-verification');
+                
+                setSubmitting(false);
+            }, 2000);
         },
-        onSubmit: (values) => {
-            setFormikData(values);
-            console.log('Form submitted:', values);
-            navigate('/login-account-verification')
-        }
     });
 
+    console.log(startFormik.isSubmitting)
+    
     return (
         <>
             <div>
                 Login get started
 
-                <form onSubmit={formik.handleSubmit}>
-                    <Form.Item name='email' label='Email'>
-                        <input
-                            placeholder='Enter your email'
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                            name="email" 
-                        />
-                    </Form.Item>
+                <form onSubmit={startFormik.handleSubmit}>
+                    <input
+                        placeholder='Enter your email'
+                        onChange={startFormik.handleChange}
+                        value={startFormik.values.email}
+                        name="email"
+                    />
 
-                    <Button block color='primary' type="submit" size='large'>
-                        Continue
+                    <Button
+                        block
+                        color='primary'
+                        type="submit"
+                        size='large'
+                        disabled={startFormik.isSubmitting}
+                    >
+                        {startFormik.isSubmitting ? 'Submitting...' : 'Continue'}
                     </Button>
-                    
                 </form>
             </div>
         </>
