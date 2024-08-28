9747aba3e29193aa60bed2b6a0ebc3f0ae937bd6
GomeniucIvan <gomeniucivan@gmail.com>
2024-08-28 22:54:27 +0300

    pers info

--- src/pages/profile/personalinformation/ProfilePersonalInformation.jsx
+++ src/pages/profile/personalinformation/ProfilePersonalInformation.jsx
@@ -1,14 +1,68 @@
 ﻿import styles from './ProfilePersonalInformation.module.less'
 import {useNavigate, useParams} from "react-router-dom";
+import PaddingBlock from "../../../components/paddingblock/PaddingBlock.jsx";
+import FormInput from "../../../form/input/FormInput.jsx";
+import FormSelect from "../../../form/formselect/FormSelect.jsx";
+import {genderList} from "../../../utils/SelectUtils.jsx";
 
 function ProfilePersonalInformation() {
     const navigate = useNavigate();
-    let { orgId } = useParams();
+    let { memberId } = useParams();
 
     return (
-        <div>
-            ProfilePersonalInformation
-        </div>
+        <PaddingBlock>
+            <FormInput label="First Name"
+                       form={formik}
+                       required={true}
+                       name='firstName'
+            />
+            <FormInput label="Last Name"
+                       form={formik}
+                       required={true}
+                       name='lastName'
+            />
+            <FormInput label="Email"
+                       form={formik}
+                       required={true}
+                       name='email'
+            />
+            <FormInput label="Username"
+                       form={formik}
+                       required={true}
+                       name='username'
+            />
+
+            <FormSelect form={formik}
+                        name={`gender`}
+                        label='Gender'
+                        options={genderList}
+                        required={false}/>
+
+            <FormInput label="Current password"
+                       form={formik}
+                       type={'password'}
+                       name='currentPassword'
+            />
+
+            <FormInput label="New password"
+                       form={formik}
+                       type={'password'}
+                       name='newPassword'
+            />
+            <FormInput label="Confirm password"
+                       form={formik}
+                       type={'password'}
+                       name='comfirmPassword'
+            />
+
+            <FormInput label="Phone number"
+                       form={formik}
+                       required={true}
+                       name='phoneNumber'
+            />
+            
+            
+        </PaddingBlock>
     )
 }
 
---
+++ src/utils/SelectUtils.jsx
@@ -0,0 +1,10 @@
+import Cookies from 'js-cookie';
+
+export const genderList = () => {
+    let list = [];
+    list.push({Text: "None", Value: 0});
+    list.push({Text: "Male", Value: 1});
+    list.push({Text: "Female", Value: 2});
+    list.push({Text: "Prefer not to disclose", Value: 2});
+    return list;
+}
