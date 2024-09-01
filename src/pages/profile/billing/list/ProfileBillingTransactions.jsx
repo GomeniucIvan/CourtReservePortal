3d0640a69b2a99d5ccfe38a6f6869c5a9b388403
GomeniucIvan <gomeniucivan@gmail.com>
2024-09-01 11:02:55 +0300

    more fixes

--- src/pages/profile/billing/list/ProfileBillingTransactions.jsx
+++ src/pages/profile/billing/list/ProfileBillingTransactions.jsx
@@ -2,19 +2,24 @@
 import {Segmented, Tabs} from "antd";
 import {useState} from "react";
 import {useApp} from "../../../../context/AppProvider.jsx";
+import {useStyles} from "../styles.jsx";
 
 function ProfileBillingTransactions() {
     const {token, globalStyles} = useApp();
     const [selectedTab, setSelectedTab] = useState(selectedTabStorage('billing_transaction', 'Unpaid'))
-    
+    const {styles} = useStyles();
+
     return (
         <>
-            <Segmented 
+            <Segmented
+                rootClassName={styles.transactionSegment}
                 options={['Unpaid', 'Paid', 'Payments', 'Adjustments', 'All']}
-                 defaultValue={selectedTab}
-                onChange={(e) => {setTabStorage('billing_transaction', e, setSelectedTab)}}
+                defaultValue={selectedTab}
+                onChange={(e) => {
+                    setTabStorage('billing_transaction', e, setSelectedTab)
+                }}
                 block
-                style={{margin : `${token.padding}px`, marginBottom: 0 }}/>
+                style={{margin: `${token.padding}px`, marginBottom: 0}}/>
         </>
     )
 }
---
+++ src/pages/profile/billing/styles.jsx
@@ -0,0 +1,30 @@
+﻿import { createStyles } from 'antd-style';
+
+export const useStyles = createStyles(({ css, token }) => ({
+    transactionSegment: css`
+        margin-top: 0 !important;
+        
+        .ant-segmented-group {
+            label:nth-child(1){
+                min-width: 60px;
+            }
+            label:nth-child(2){
+                min-width: 45px;
+            }
+            label:nth-child(3){
+                min-width: 75px;
+            }
+            label:nth-child(4){
+                min-width: 90px;
+            }
+            label:nth-child(5){
+                min-width: 40px;
+            }
+            
+            .ant-segmented-item-label {
+                padding: 0 6px;
+            }
+        }
+        
+  `,
+}));
