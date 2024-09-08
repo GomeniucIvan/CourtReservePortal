47a904a2728f767091b24cd992b0a7004ed06596
GomeniucIvan <gomeniucivan@gmail.com>
2024-09-08 12:41:18 +0300

    fix form loading

--- src/form/formcustomfields/FormCustomFields.jsx
+++ src/form/formcustomfields/FormCustomFields.jsx
@@ -1,15 +1,28 @@
-import {Radio, Typography} from 'antd';
-import {isNullOrEmpty} from "../../utils/Utils.jsx";
+import {Radio, Skeleton, Typography} from 'antd';
+import {isNullOrEmpty, toBoolean} from "../../utils/Utils.jsx";
 import FormInput from "../input/FormInput.jsx";
 import FormTextArea from "../formtextarea/FormTextArea.jsx";
 import FormSelect from "../formselect/FormSelect.jsx";
+import {cx} from "antd-style";
+import {useApp} from "../../context/AppProvider.jsx";
 const {Title} = Typography;
 
 const FormCustomFields = ({ customFields, form, keyPrefix, loading }) => {
+    const {globalStyles} = useApp();
+    
     if (isNullOrEmpty(customFields)) {
         return null;
     }
 
+    if (toBoolean(loading)){
+        return (
+            <div className={cx(globalStyles.formBlock)}>
+                <Skeleton.Input block active={true} className={cx(globalStyles.skeletonLabel) }/>
+                <Skeleton.Input block active={true} className={cx(globalStyles.skeletonInput) }/>
+            </div>
+        )
+    }
+    
     return customFields.map(({ UdfType, FieldType, Label, Id, IsRequired, Options }) => {
         const name = isNullOrEmpty(keyPrefix) ? `udf_${Id}` : `${keyPrefix}.udf_${Id}`;
         const type = UdfType || FieldType;
@@ -22,7 +35,7 @@ const FormCustomFields = ({ customFields, form, keyPrefix, loading }) => {
                         label={Label}
                         name={name}
                         loading={loading}
-                        placeholder={Label}
+                        //placeholder={Label}
                         required={IsRequired}
                         form={form}
                     />
@@ -35,7 +48,7 @@ const FormCustomFields = ({ customFields, form, keyPrefix, loading }) => {
                         label={Label}
                         name={name}
                         loading={loading}
-                        placeholder={Label}
+                        //placeholder={Label}
                         required={IsRequired}
                         form={form}
                     />
--- src/form/formratingcategories/FormRatingCategories.jsx
+++ src/form/formratingcategories/FormRatingCategories.jsx
@@ -1,11 +1,25 @@
-import {isNullOrEmpty} from "../../utils/Utils.jsx";
+import {isNullOrEmpty, toBoolean} from "../../utils/Utils.jsx";
 import FormSelect from "../formselect/FormSelect.jsx";
+import {cx} from "antd-style";
+import {Skeleton} from "antd";
+import {useApp} from "../../context/AppProvider.jsx";
 
 const FormRatingCategories = ({ form, ratingCategories, keyPrefix, loading }) => {
+    const {globalStyles} = useApp();
+    
     if (isNullOrEmpty(ratingCategories)) {
         return null;
     }
 
+    if (toBoolean(loading)){
+        return (
+            <div className={cx(globalStyles.formBlock) }>
+                <Skeleton.Input block active={true} className={cx(globalStyles.skeletonLabel) }/>
+                <Skeleton.Input block active={true} className={cx(globalStyles.skeletonInput) }/>
+            </div>
+        )
+    }
+    
     return ratingCategories.map(({ AllowMultipleRatingValues, Ratings, Name, Id, IsRequired }) => {
         const name = isNullOrEmpty(keyPrefix) ? `rat_${Id}` : `${keyPrefix}.rat_${Id}`;
         
